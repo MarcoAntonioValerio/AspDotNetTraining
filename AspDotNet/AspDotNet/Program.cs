17a32e1b53dbbc6df322a27a4ad44cd6ca6e3b6e
Mav <marco.valerio@hotmail.it>
2020-01-12 21:12:15 +0000

    Commiting stuff

--- AspDotNet/AspDotNet/Program.cs
+++ AspDotNet/AspDotNet/Program.cs
@@ -10,30 +10,17 @@ namespace AspDotNet
     {
         static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int answer = 0;
-
-            Console.WriteLine("Enter a number.");
-            string sNum1 = Console.ReadLine();
-
-            Console.WriteLine("Enter another number.");
-            string sNum2 = Console.ReadLine();
-
-            Int32.TryParse(sNum1, out num1);
-            Int32.TryParse(sNum2, out num2);
-
-            answer = num1 + num2;
-
-            Console.WriteLine("The answer is " + answer); 
-
-            Console.ReadLine();
+            
         }
+    }
 
-        static void WriteLine(string value)
-        {
+    class PetClass
+    {
 
+    }
 
-        }
+    struct PetStruct
+    {
+
     }
 }
